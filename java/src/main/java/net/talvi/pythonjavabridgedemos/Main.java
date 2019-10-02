6c997b14bd82136d2dc5b94415050727d6450aea
Pontus Lurcock <pont@talvi.net>
2019-10-02 19:56:23 +0200

    Add main method and jepscript option
     - Add a main method to the Main class.
    
     - Implement a jepscript option to run Python scripts using Jep.
    
     - Update the POM to add Jep and commons-cli dependencies and a
       combined jar build goal using make-assembly.

--- java/src/main/java/net/talvi/pythonjavabridgedemos/Main.java
+++ java/src/main/java/net/talvi/pythonjavabridgedemos/Main.java
@@ -1,5 +1,16 @@
 package net.talvi.pythonjavabridgedemos;
 
+import jep.Interpreter;
+import jep.JepException;
+import jep.SharedInterpreter;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+
 public class Main {
     
     public String name;
@@ -16,4 +27,33 @@ public class Main {
         return greeting + ", " + name + "!";
     }
     
+    public static void runJepScript(String filename) {
+        try (Interpreter interp = new SharedInterpreter()) {
+            interp.runScript(filename);
+        } catch (JepException ex) {
+            System.err.println("Jep exception:\n"
+                    + ex.getMessage());
+            System.exit(1);
+        }
+    }
+    
+    public static void main(String[] args) {
+        final Options options = new Options();
+        
+        options.addOption(Option.builder("jepscript")
+            .hasArg().argName("file").desc("run a Python script with Jep")
+            .build());
+        final CommandLineParser parser = new DefaultParser();
+        
+        try {
+            final CommandLine commandLine = parser.parse(options, args);
+            if (commandLine.hasOption("jepscript")) {
+                runJepScript(commandLine.getOptionValue("jepscript"));
+            }
+        } catch (ParseException ex) {
+            System.err.println("Could not parse arguments.\n"
+                    + ex.getMessage());
+            System.exit(1);
+        } 
+    }
 }
