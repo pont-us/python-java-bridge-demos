798421d9a3babcfdb547dde8dc8289c31ee076b1
Pontus Lurcock <pont@talvi.net>
2019-10-25 15:56:58 +0200

    Add support for Jython, etc.
     - Add a -jythonscript option and corresponding method runJythonScript to
       Main.
    
     - Add jython to the dependencies in pom.xml.
    
     - In Main::runJepScript, bind a variable "main" to an instance of Main,
       to test use of pre-bound variables in called scripts.

--- java/src/main/java/net/talvi/pythonjavabridgedemos/Main.java
+++ java/src/main/java/net/talvi/pythonjavabridgedemos/Main.java
@@ -1,5 +1,12 @@
 package net.talvi.pythonjavabridgedemos;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import jep.Interpreter;
 import jep.JepException;
 import jep.SharedInterpreter;
@@ -29,6 +36,7 @@ public class Main {
     
     public static void runJepScript(String filename) {
         try (Interpreter interp = new SharedInterpreter()) {
+            interp.set("main", new Main("Jeff"));
             interp.runScript(filename);
         } catch (JepException ex) {
             System.err.println("Jep exception:\n"
@@ -37,11 +45,24 @@ public class Main {
         }
     }
     
-    public static void main(String[] args) {
+    public static void runJythonScript(String filename)
+            throws FileNotFoundException, IOException, ScriptException {
+        final ScriptEngine scriptEngine =
+                new ScriptEngineManager().getEngineByName("python");
+        scriptEngine.put("main", new Main("Jeff"));
+        try (Reader reader = new FileReader(filename)) {
+            scriptEngine.eval(reader);
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
         final Options options = new Options();
         
         options.addOption(Option.builder("jepscript")
             .hasArg().argName("file").desc("run a Python script with Jep")
+            .build());
+        options.addOption(Option.builder("jythonscript")
+            .hasArg().argName("file").desc("run a Python script with Jython")
             .build());
         final CommandLineParser parser = new DefaultParser();
         
@@ -49,6 +70,9 @@ public class Main {
             final CommandLine commandLine = parser.parse(options, args);
             if (commandLine.hasOption("jepscript")) {
                 runJepScript(commandLine.getOptionValue("jepscript"));
+            }
+            if (commandLine.hasOption("jythonscript")) {
+                runJythonScript(commandLine.getOptionValue("jythonscript"));
             }
         } catch (ParseException ex) {
             System.err.println("Could not parse arguments.\n"
