a340ddc467dcc21d6bee62f1d40570ca0eaa75dc
Pontus Lurcock <pont@talvi.net>
2020-01-09 10:33:45 +0100

    Improve comments in scripts; tweak Jpype output
     - Update comments to use the term "demonstrate" rather than "test",
       since the latter may be interpreted as referring to automated
       testing.
    
     - Print a more informative demonstration string in jpype-script.py.

--- python/jpype-script.py
+++ python/jpype-script.py
@@ -9,7 +9,7 @@ jpype.startJVM(jpype.getDefaultJVMPath(),
                "-Djava.class.path=" + jar_path,
                convertStrings=False)
 
-jpype.java.lang.System.out.println("test")
+jpype.java.lang.System.out.println("This was printed via System.out.println.")
 
 main = jpype.JPackage("net").talvi.pythonjavabridgedemos.Main("Jeff")
 print(main.greet("Morning"))
--- python/py4j-script.py
+++ python/py4j-script.py
@@ -4,7 +4,7 @@ from py4j.java_gateway import JavaGateway
 
 gateway = JavaGateway()
 
-# Test use of standard Java classes.
+# Demonstrate use of standard Java classes.
 
 cal = gateway.jvm.java.util.\
     GregorianCalendar(2008, gateway.jvm.java.util.Calendar.DECEMBER, 3)
@@ -13,11 +13,11 @@ weekday = cal.getDisplayName(gateway.jvm.java.util.Calendar.DAY_OF_WEEK,
                              gateway.jvm.java.util.Locale.ENGLISH)
 print("Python 3 was released on a {}.".format(weekday))
 
-# Test use of the class provided as an entry point.
+# Demonstrate use of the class provided as an entry point.
 
 print(gateway.entry_point.greet("Salutations"))
 
-# Test instantiation and use of a third-party class.
+# Demonstrate instantiation and use of a third-party class.
 
 main = gateway.jvm.net.talvi.pythonjavabridgedemos.Main("Trismestigus")
 print(main.greet("Felicitations"))
